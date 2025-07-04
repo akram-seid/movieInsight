35168c00789cc0acab1992aea6b249fc4e4d8b03
Akram Seid Endris <akiseid@gmail.com>
2025-07-04 16:09:51 +0200

    Initial commit

---
+++ src/main/java/com/seidbros/movieinsight/MovieInsightApplication.java
@@ -0,0 +1,13 @@
+package com.seidbros.movieinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInsightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieInsightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seidbros/movieinsight/MovieInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seidbros.movieinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInsightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
